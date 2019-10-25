006ce3e313de2b7df03294ce5be626c944bd8bf3
juliacanas <julia_cl90@homtail.com>
2019-10-25 13:53:15 +0200

    starting partials

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const userSchema = new Schema({
   username: {type: String, required: true, unique: true},
   password: {type: String, required: true}, 
   motivSentence: {type: String, default: "This is my super awesome motivational sentence."},
-  imgPath: String,
+  imgPath: { type: String, default: "http://s3.amazonaws.com/37assets/svn/765-default-avatar.png"},
   hp: {type: Number, default: 50},  //Stands for Health points
   exp: {type: Number, default: 0},  //Stands for exp.
   personalChallenges: [ { type: Schema.Types.ObjectId, ref: "PersonalChallenge"} ],
