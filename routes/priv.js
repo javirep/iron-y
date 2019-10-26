85a1b46276018da5e24999d7f7af71df598af6d2
Javier Repetto de los Dolores <javier.repettodelosdolres@gmail.com>
2019-10-26 20:47:47 +0200

    add a personal challenge working

--- routes/priv.js
+++ routes/priv.js
@@ -44,7 +44,7 @@ router.post("/addPersonalChallenge", (req, res, next) => {
     console.log(personalChallenge.name + " saved in the DB.");
   });
 
-  User.findOneAndUpdate({ "_id": req.session.currentUser._id }, { $set: { personalChallenges: personalChallenge._id } }, { new: true })
+  User.findOneAndUpdate({ "_id": req.session.currentUser._id }, { $push: { personalChallenges: personalChallenge._id } }, { new: true })
     .then(user => console.log(user))
 
   res.redirect("/priv/user/" + req.session.currentUser._id)
