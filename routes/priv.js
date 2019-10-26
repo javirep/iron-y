4b490c0aa73ab68bee54c9f6da05a8b3213efa07
Javier Repetto de los Dolores <javier.repettodelosdolres@gmail.com>
2019-10-26 22:19:38 +0200

    personal Challenge see details working!

--- routes/priv.js
+++ routes/priv.js
@@ -18,22 +18,15 @@ router.use((req, res, next) => {
 
 router.get('/user', async (req, res, next) => {
   const id = req.session.currentUser._id
-  const user = await User.findOne({ "_id": id })
-    .populate("personalChallenges")
-  console.log(user.personalChallenges)
+  const user = await User.findOne({ "_id": id }).
+    populate("personalChallenges")
   res.render("priv/profile.hbs", { user })
 });
-
-router.get('/user/challenge/:id', async (req, res, next) => {
-  res.render("priv/personalChallengeDetail.hbs")
-})
-
 
 /* GET users listing. */
 router.get("/addPersonalChallenge", (req, res, next) => {
   res.render("priv/addPersonalChallenge.hbs")
 })
-
 
 router.post("/addPersonalChallenge", (req, res, next) => {
   const { name, description, modify, difficulty } = req.body
@@ -53,7 +46,13 @@ router.post("/addPersonalChallenge", (req, res, next) => {
   res.redirect("/priv/user/")
 })
 
-
+router.get("/user/personalChallenge/:id", (req, res, next) => {
+  const { id } = req.params;
+  PersonalChallenge.findOne({ "_id": id })
+    .then(personalChallenge => {
+      res.render("priv/personalChallengeDetail.hbs", { personalChallenge })
+    })
+})
 
 router.get("/socialChallenges", (req, res, next) => {
   SocialChallenge.find()
@@ -74,16 +73,17 @@ router.get('/socialChallengeDetail/:id', (req, res, next) => {
 })
 
 
-//cuando se clica a JOIN: buscar id del socialChallenge concreto. Añadir ese objeto a User.socialChallenges
-router.post('/addSocialChallenge/:id', (req, res, next) => {
-  console.log('Hola')
-  const { id } = req.params;
-  const userId = req.session.currentUser;
-  SocialChallenge.findById(id)
-  User.findOneAndUpdate({ "_id": userId}, { $push: { socialChallenges: id } }, { new: true })
-    .then(challenge => console.log(challenge))
-        res.redirect("/priv/socialChallenges");
-})
+// cuando se clica a JOIN: buscar id del socialChallenge concreto. Añadir ese objeto a User.socialChallenges
+// router.get('/priv/addSocialChallenge/:id', (req, res, next) => {
+//   const { id } = req.params;
+//   const userId = req.session.currentUser._id;
+//   SocialChallenge.findById(id)
+//   User.findOneAndUpdate({ "_id": userId }, { $set: { socialChallenges: _id } }, { new: true })
+//     .then(challenge => console.log(challenge))
+
+//   res.render("/priv/socialChallenges")
+
+// })
 
 
 
