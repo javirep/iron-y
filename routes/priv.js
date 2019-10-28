027e24b2a09f9f7c37caf76e5ecd7ee25b7a769e
juliacanas <julia_cl90@homtail.com>
2019-10-28 13:07:01 +0100

    social challenges show correct social challenges

--- routes/priv.js
+++ routes/priv.js
@@ -22,7 +22,7 @@ router.get('/user', async (req, res, next) => {
   const user = await User.findOne({ "_id": id })
     .populate("personalChallenges")
     .populate("socialChallenges")
-
+  console.log(user)
   res.render("priv/profile.hbs", { user })
 });
 
@@ -97,14 +97,31 @@ router.get("/user/personalChallenge/:id", (req, res, next) => {
 
 
 
-router.get("/socialChallenges", (req, res, next) => {
-  SocialChallenge.find()
-    .then(data => {
-      res.render("priv/socialChallenges.hbs", { data })
-      console.log(data)
-    })
+router.get("/socialChallenges", async (req, res, next) => {
+  try {
 
-    .catch(error => { console.log('Error finding socialChallenge', error) })
+    const { _id } = req.session.currentUser
+    const data = await SocialChallenge.find()
+    const challengesFromUser = await User.findOne({ _id })
+    const newChallenges = []
+
+    for (challenge of data) {
+      let iHaveIt = false
+      challengesFromUser.socialChallenges.forEach(userC => {
+        if (challenge._id.equals(userC._id)) {
+          iHaveIt = true
+        }
+      })
+      if (!iHaveIt) {
+        newChallenges.push(challenge)
+      }
+    }
+    res.render("priv/socialChallenges.hbs", { newChallenges })
+  }
+  catch (error) {
+    console.log('Error finding socialChallenge', error)
+  }
+
 })
 
 
@@ -126,13 +143,12 @@ router.post('/addSocialChallenge/:id', (req, res, next) => {
     .catch(error => { console.log(error) })
 })
 
-//NO FUNCIONA!!
 
 router.post('/socialChallenge/delete/:id', (req, res, next) => {
   const { id } = req.params;
   const userId = req.session.currentUser._id;
-  User.findByIdAndUpdate({"_id": userId}, {$pull: {socialChallenges: id}})
-  .then(() => res.redirect('/priv/user/'))
+  User.findByIdAndUpdate({ "_id": userId }, { $pull: { socialChallenges: id } })
+    .then(() => res.redirect('/priv/user/'))
 });
 
 
