af6e0dda4ae82d022b81a9ca5032cc05e381e03f
juliacanas <julia_cl90@homtail.com>
2019-10-30 12:22:53 +0100

    working on editProfile route

--- routes/priv.js
+++ routes/priv.js
@@ -257,5 +257,8 @@ router.post("/failedSocialChallenge/:id", async (req, res, next) => {
   res.redirect("/priv/user")
 })
 
+router.get("/editProfile", (req, res, next) => {
+  res.render('priv/editProfile.hbs')
+})
 
 module.exports = router;
